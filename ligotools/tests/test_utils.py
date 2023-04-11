6181839e603396f174e1f4ac8449d60345f67e06
kanglin820 <kanglin@berkeley.edu>
2023-04-11 22:44:02 +0000

    create test_utils.py for tests four functions of utils.py

---
+++ ligotools/tests/test_utils.py
@@ -0,0 +1,25 @@
+# 4 tests on utils.py
+
+from ligotools import utils
+import pytest
+import numpy as np
+
+
+def test_whiten():
+"""test function test_write
+"""    
+
+#def test_write_wavfile():
+#    """test function write_wavfile
+#    """
+    
+
+#def test_reqshift():
+#    """test function reqshift
+#    """
+    
+    
+#def test_plot_psd():
+#    """test function plot_psd
+#    """
+
